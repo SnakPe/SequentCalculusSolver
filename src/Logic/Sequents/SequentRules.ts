44768cb3eefdcae2c29edfc4d34ea7432093df0c
SnakPe <96241570+SnakPe@users.noreply.github.com>
2025-06-17 00:04:53 +0200

    Added GPLv3 Licence

--- src/Logic/Sequents/SequentRules.ts
+++ src/Logic/Sequents/SequentRules.ts
@@ -1,3 +1,22 @@
+/*
+
+Rules of the sequent calculus
+Copyright (C) 2025 Andreas Kovalski
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 /**
  * For all rules : big greek letters are sequences of formulas, small greek letters are formulas. Sequences are also allowed to be empty
--- src/Render.ts
+++ src/Render.ts
@@ -1,3 +1,23 @@
+/*
+
+Renderen for sequent calculus proofs
+Copyright (C) 2025 Andreas Kovalski
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 type Renderer<ElementType extends Element> = {
   renderProof : (proof : ProofTree) => ElementType,
   drawProofSplitBorders : (proofElement : ElementType) => void,
